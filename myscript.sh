1b269bfa372a0570a1f247486c34ea731eae4890
Rodney Odvina <rodneyodvina@Rodneys-MacBook-Pro.local>
2019-01-27 12:01:10 -0500

    Add creds param

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,10 @@
-echo "hello $1"
+#!/bin/bash
+
+env=$1
+echo "env = $env";
+
+# source ./org.$env.properties
+
+admin=$2;
+echo "creds=$admin";
+curl -X GET https://api.enterprise.apigee.com/v1/organizations/rodvina-eval/companies -H 'Content-Type: application/json' -u $admin
