300b55c7b3d32cfd89d9999e3d900d1d7379032f
Rodney Odvina <rodvina@gmail.com>
2019-01-28 07:13:39 -0500

    jenkinsfile-new runscript

---
+++ myscript.1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+env=$1
+echo "env = $env";
+
+# source ./org.$env.properties
+
+admin=$2;
+echo "creds=$admin";
+response=$(curl -X GET https://api.enterprise.apigee.com/v1/organizations/rodvina-eval/companies -H 'Content-Type: application/json' -u $admin)
+echo "response=$response";
